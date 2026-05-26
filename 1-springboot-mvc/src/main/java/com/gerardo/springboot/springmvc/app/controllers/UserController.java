000affb55ce27def61c21a003e2f1fb84dbccf53
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-05-25 23:54:27 -0600

    Creando Proyecto spring web MVC

---
+++ 1-springboot-mvc/src/main/java/com/gerardo/springboot/springmvc/app/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.gerardo.springboot.springmvc.app.controllers;
+
+import com.gerardo.springboot.springmvc.app.entities.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping({"/view","/"})
+//    @RequestMapping("/app")
+    public String view(Model model){
+        model.addAttribute("title","Hola Mundo Spring Boot");
+        model.addAttribute("message","Esta es una aplicacion de ejemplo usando Spring");
+        model.addAttribute("user",new User("Gerardo","Lara"));
+        return "view"; // aqui va el nombre de la plantilla, tiene que ser el mismo
+    }
+}
