b0df11085b1fdeed0c035873878c3508de9321b6
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-11 00:06:39 -0600

    Iniciando nuevo curso de Spring

---
+++ springboot-web/src/test/java/com/gerardo/curso/webapp/SpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerardo.curso.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
