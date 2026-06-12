31e330fcaaaf016e75fdd13977c3b3de1734a24f
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-12 12:56:22 -0600

    Pasar datos a la vista

--- springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserController.java
+++ springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserController.java
@@ -1,13 +1,20 @@
 package com.gerardo.curso.webapp.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    // Usando Model
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+        model.addAttribute("title", "Hola mundo Spring");
+        model.addAttribute("name","Gerardo");
+        model.addAttribute("lastname","Lara");
         return "details";
     }
 }
