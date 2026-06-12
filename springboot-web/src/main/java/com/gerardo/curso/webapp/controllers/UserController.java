533a419acb84124c25a34f4834a57db213e53a62
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-12 16:07:45 -0600

    Anotacion @RequestMapping

--- springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserController.java
+++ springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserController.java
@@ -3,8 +3,10 @@ package com.gerardo.curso.webapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/web")
 public class UserController {
 
     // Usando Model
--- springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserRestController.java
@@ -4,13 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
     // Usando Model
-    @GetMapping("/details2")
+    @GetMapping("/details")
+    // Tambien podemos usar
+    // @RequestMapping(path = "/details", method = RequestMethod.GET)
     public Map<String, Object> details(){
         Map<String, Object> body = new HashMap<>();
         body.put("title", "Hola mundo Spring");
