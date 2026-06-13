c2e8573556c765937d0a34386e0ac1a776cf7058
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-12 19:24:58 -0600

    La clase Dto

--- springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gerardo.curso.webapp.models.User;
+import com.gerardo.curso.webapp.models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
@@ -17,11 +18,22 @@ public class UserRestController {
     @GetMapping("/details")
     // Tambien podemos usar
     // @RequestMapping(path = "/details", method = RequestMethod.GET)
-    public Map<String, Object> details(){
+    public UserDto details(){
+        User user = new User("Gerardo","Lara");
+        UserDto userDto = new UserDto("Hola mundo",user);
+
+        return userDto;
+    }
+
+      // Usando Model
+    @GetMapping("/details-map")
+    // Tambien podemos usar
+    // @RequestMapping(path = "/details", method = RequestMethod.GET)
+    public Map<String, Object> detailsMap(){
         Map<String, Object> body = new HashMap<>();
         User user = new User("Gerardo","Lara");
 
-        body.put("title", "Hola mundo Spring");
+        body.put("title", "Hola mundo Spring con Map");
         body.put("user",user);
         return body;
     }
