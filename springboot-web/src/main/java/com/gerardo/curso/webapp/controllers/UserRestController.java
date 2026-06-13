1f32e6588a284a5a17908c31b1e37139aa021408
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-12 19:41:59 -0600

    Api Rest Listar

--- springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.gerardo.curso.webapp.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +26,22 @@ public class UserRestController {
         UserDto userDto = new UserDto("Hola mundo",user);
 
         return userDto;
+    }
+
+    @GetMapping("/list")
+    public List<User> list(){
+        User user1 = new User("Gerardo","Lara");
+        User user2 = new User("Andres","Garcia");
+        User user3 = new User("Gaby","Ortiz");
+
+        List<User> users = Arrays.asList(user1,user2,user3);
+
+        // List<User> users = new ArrayList<>();
+        // users.add(user1);
+        // users.add(user2);
+        // users.add(user3);
+
+        return users;
     }
 
       // Usando Model
