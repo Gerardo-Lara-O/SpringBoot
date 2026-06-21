c5e0fc27c2f9d744a847d5c3d77ec3670fb1a273
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-21 16:34:00 -0600

    Inyectando valores usando la anotacion @Value

--- springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.gerardo.curso.webapp.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,18 @@ import com.gerardo.curso.webapp.models.dto.ParamDtoMix;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    // @Value("${config.message}")
+    // private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
 
     @GetMapping("/baz/{message}")
     public ParamDtoMix baz(@PathVariable String message){
@@ -37,6 +50,16 @@ public class PathVariableController {
     public User create(@RequestBody User user){
 
         return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String,Object> values(@Value("${config.message}") String message){
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code",code);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        return json;
     }
     
 }
