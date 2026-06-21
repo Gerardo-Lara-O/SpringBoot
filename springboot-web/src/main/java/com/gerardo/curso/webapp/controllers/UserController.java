3f50a74f7a8f6b4f9ceb4921c7aded6383cb19ae
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-20 21:28:32 -0600

    Anotacion @ModelAttribute

--- springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserController.java
+++ springboot-web/src/main/java/com/gerardo/curso/webapp/controllers/UserController.java
@@ -1,16 +1,16 @@
 package com.gerardo.curso.webapp.controllers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gerardo.curso.webapp.models.User;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -32,17 +32,20 @@ public class UserController {
 
     @GetMapping("/list")
     public String listString(Model model) {
-        List<User> users = Arrays.asList(
+        model.addAttribute("title","Listado de usuarios");
+
+        return "list";
+    }
+
+        // Ahora users ya no lo cargo en el model
+    @ModelAttribute("users")
+    public List<User> usersModel(){
+        return Arrays.asList(
             new User("Pepa","Gonzales","pepa@email.com"), 
             new User("Lalo", "Perez","lalo@email.com"),
             new User("Juanita","Roe","juana@email.com"),
             new User("Andres","Doe")
         );
-
-        model.addAttribute("title","Listado de usuarios");
-        model.addAttribute("users",users);
-
-        return "list";
     }
     
 }
