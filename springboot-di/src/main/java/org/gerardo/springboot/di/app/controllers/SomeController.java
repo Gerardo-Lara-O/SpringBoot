afcd04743b7ed0d5dcef55ebc0b9a21aad6add1b
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-22 03:24:21 -0600

    Creando la clase controller

---
+++ springboot-di/src/main/java/org/gerardo/springboot/di/app/controllers/SomeController.java
@@ -0,0 +1,27 @@
+package org.gerardo.springboot.di.app.controllers;
+
+import java.util.List;
+
+import org.gerardo.springboot.di.app.models.Product;
+import org.gerardo.springboot.di.app.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+
+    private ProductService service = new ProductService();
+
+    @GetMapping
+    public List<Product> list(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id){
+        return service.findById(id);
+    }
+}
