31314e626742dfab0df0b81246e149f2b214fa9b
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-24 09:34:39 -0600

    Factura y relaciones entre sus componentes

--- springboot-difactura/src/main/java/org/gerardo/curso/springboot/di/factura/models/Client.java
+++ springboot-difactura/src/main/java/org/gerardo/curso/springboot/di/factura/models/Client.java
@@ -1,5 +1,34 @@
 package org.gerardo.curso.springboot.di.factura.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Client {
+
+    private String name;
+    private String lastname;
     
+    // Constructor
+    public Client(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    // Getters y Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
 }
--- springboot-difactura/src/main/java/org/gerardo/curso/springboot/di/factura/models/Item.java
+++ springboot-difactura/src/main/java/org/gerardo/curso/springboot/di/factura/models/Item.java
@@ -1,5 +1,27 @@
 package org.gerardo.curso.springboot.di.factura.models;
 
 public class Item {
+
+    private Product product;
+    private Integer quantity;
+
+
+    // Getters y Setters
+ public Product getProduct() {
+    return product;
+ }
+
+ public void setProduct(Product product) {
+    this.product = product;
+ }
+
+ public Integer getQuantity() {
+    return quantity;
+ }
+
+ public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+ }
+    
     
 }
