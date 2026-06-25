4518e744929eeca3e6afdb1274ba473d1f7d9a0f
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-25 03:42:53 -0600

    Lanzamiento de excepcion usando Api Optional

--- springboot-error/src/main/java/com/gerardo/curso/springboot/error/controllers/AppController.java
+++ springboot-error/src/main/java/com/gerardo/curso/springboot/error/controllers/AppController.java
@@ -1,6 +1,9 @@
 package com.gerardo.curso.springboot.error.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,18 +27,20 @@ public class AppController {
     @GetMapping
     public String index(){
         // int value = 100/0;
-        int value = Integer.parseInt("10");
+        int value = Integer.parseInt("10x");
         System.out.println(value);
         return "ok 200";
     }
 
     @GetMapping("/show/{id}")
-    public User show(@PathVariable(name = "id") Long id){
-        User user = service.findById(id);
-        if (user == null) {
-            throw new UserNotFoundException("Error el usuario no existe!");
+    public ResponseEntity<?> show(@PathVariable Long id){
+        // User user = service.findById(id).orElseThrow(() -> new UserNotFoundException("Error el usuario no existe!"));
+        Optional<User> optionalUser = service.findById(id);
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
         }
-        System.out.println(user.getLastName());
-        return user;
+
+        // System.out.println(user.getLastName());
+        return ResponseEntity.ok(optionalUser.get());
     }
 }
--- springboot-error/src/main/java/com/gerardo/curso/springboot/error/services/UserService.java
+++ springboot-error/src/main/java/com/gerardo/curso/springboot/error/services/UserService.java
@@ -1,11 +1,12 @@
 package com.gerardo.curso.springboot.error.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.gerardo.curso.springboot.error.models.domain.User;
 
 public interface UserService {
     
     List<User> findAll();
-    User findById(Long id);
+    Optional<User> findById(Long id);
 }
