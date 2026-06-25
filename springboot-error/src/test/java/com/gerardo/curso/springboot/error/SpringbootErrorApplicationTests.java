2f7bf8e1b2e13b39a72a212659df58ea6387d5b1
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-24 18:20:09 -0600

    Creando nuevo proyecto - springboot-error

---
+++ springboot-error/src/test/java/com/gerardo/curso/springboot/error/SpringbootErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerardo.curso.springboot.error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
