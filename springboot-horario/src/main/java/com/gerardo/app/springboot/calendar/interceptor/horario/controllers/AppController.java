8a2819fc36aef50ecb11c5a129d063f3afbf39fa
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-25 21:50:11 -0600

    Creando nuevo proyecto - springboot-horario

---
+++ springboot-horario/src/main/java/com/gerardo/app/springboot/calendar/interceptor/horario/controllers/AppController.java
@@ -0,0 +1,23 @@
+package com.gerardo.app.springboot.calendar.interceptor.horario.controllers;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    
+    @GetMapping("/foo")
+    public ResponseEntity<?> foo(){
+        Map<String, Object> data = new HashMap<>();
+        data.put("title", "Bienvenidos al sistema de atencion");
+        data.put("time", new Date());
+
+        return ResponseEntity.ok(data);
+    }
+}
---
+++ springboot-horario/src/test/java/com/gerardo/app/springboot/calendar/interceptor/horario/SpringbootHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerardo.app.springboot.calendar.interceptor.horario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
