7d4a446091259ca964082b7a782f149ecd8bb04a
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-25 19:11:45 -0600

    Creando nuevo proyecto - springboot-interceptor

---
+++ springboot-interceptor/src/test/java/com/gerardo/curso/springboot/app/interceptor/SpringbootInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerardo.curso.springboot.app.interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
