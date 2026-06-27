2abf406d857d0964119919cb91c741b395554d44
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-06-26 19:40:53 -0600

    Advice After y expresiones regulares en pointcut

--- springboot-aop/src/main/java/com/gerardo/curso/springboot/app/aop/aop/GreetingAspect.java
+++ springboot-aop/src/main/java/com/gerardo/curso/springboot/app/aop/aop/GreetingAspect.java
@@ -3,6 +3,7 @@ package com.gerardo.curso.springboot.app.aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -14,11 +15,19 @@ import org.springframework.stereotype.Component;
 public class GreetingAspect {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(String com.gerardo.curso.springboot.app.aop.services.GreetingService.sayHello(..))")
+    @Before("execution(String com.gerardo.curso.springboot.app.aop..*.*(..))")
     public void loggerBefore(JoinPoint joinPoint){
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
 
         logger.info("\nAntes: " + method + " con los argumentos " + args);
+    }
+
+    @After("execution(String com.gerardo.curso.springboot.app.aop..*.*(..))")
+    public void loggerAfter(JoinPoint joinPoint){
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("\nDespues: " + method + " con los argumentos " + args);
     }
 }
