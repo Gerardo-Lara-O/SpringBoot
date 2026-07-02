35c5ce16620b33c5c6624294e21fec9b256150ec
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-07-01 23:26:10 -0600

    Obtener un solo objeto con findById

--- springboot-jpa/src/main/java/com/gerardo/curso/springboot/jpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/gerardo/curso/springboot/jpa/SpringbootJpaApplication.java
@@ -1,6 +1,7 @@
 package com.gerardo.curso.springboot.jpa;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -24,7 +25,23 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// aqui vamos a trabajar con nuestra persistancia
+		findOne();
+		
+	}
 
+	public void findOne(){
+		Person person = null;
+		Optional<Person> optionalPerson = repository.findById(1L);
+		if (optionalPerson.isPresent()) {
+			person = optionalPerson.get();
+		}
+		System.out.println(person);
+
+		// o usamos una abreviacion
+		// repository.findById(1L).ifPresent(person -> System.out.println(person));
+	}
+
+	public void list(){
 		// List<Person> persons = (List<Person>)repository.findAll();
 		List<Person> persons = (List<Person>)repository.findByProgrammingLanguageAndName("Java","Gerardo");
 		persons.stream().forEach(person -> {
