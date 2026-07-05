1e244c64b8bbbe6c051677ba514d01d28c96a091
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-07-05 17:57:14 -0600

    Creando nuevo proyecto - asociaciones

---
+++ springboot-jpa-relationship/src/main/java/com/gerardo/curso/springboot/jpa/models/Client.java
@@ -0,0 +1,71 @@
+package com.gerardo.curso.springboot.jpa.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastname;
+    
+    // Constructor
+    public Client() {
+    }
+
+    public Client(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public Client(Long id, String name, String lastname) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Client [id=" + id + ", name=" + name + ", lastname=" + lastname + "]";
+    }
+
+    // Metodos
+    
+    
+
+    
+
+}
---
+++ springboot-jpa-relationship/src/main/java/com/gerardo/curso/springboot/jpa/models/Invoice.java
@@ -0,0 +1,70 @@
+package com.gerardo.curso.springboot.jpa.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "invoices")
+public class Invoice {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Long total;
+
+    // Constructores
+    public Invoice() {
+    }
+    
+    public Invoice(String description, Long total) {
+        this.description = description;
+        this.total = total;
+    }
+
+    public Invoice(Long id, String description, Long total) {
+        this.id = id;
+        this.description = description;
+        this.total = total;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice [id=" + id + ", description=" + description + ", total=" + total + "]";
+    }
+
+    // Metodos
+
+    
+
+
+}
