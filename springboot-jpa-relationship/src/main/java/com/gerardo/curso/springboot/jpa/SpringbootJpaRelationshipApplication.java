d8dae6be06804597dda476bf661efcc8ee3953c5
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-07-07 04:04:19 -0600

    Codificando ejemplo OneToMany Bidireccional

--- springboot-jpa-relationship/src/main/java/com/gerardo/curso/springboot/jpa/SpringbootJpaRelationshipApplication.java
+++ springboot-jpa-relationship/src/main/java/com/gerardo/curso/springboot/jpa/SpringbootJpaRelationshipApplication.java
@@ -1,6 +1,8 @@
 package com.gerardo.curso.springboot.jpa;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +35,7 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		removeAddressFindById();
+		oneToManyInvoiceBidireccional();
 	}
 
 	@Transactional
@@ -137,6 +139,26 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner{
 			});
 		});
 
+	}
+
+
+	@Transactional
+	public void oneToManyInvoiceBidireccional(){
+		Client client = new Client("Fran","Moras");
+
+		Invoice invoice1 = new Invoice("Compras de la casa",5000L);
+		Invoice invoice2 = new Invoice("Compras de oficina",8000L);
+
+		List<Invoice> invoices = new ArrayList<>();
+		invoices.add(invoice1);
+		invoices.add(invoice2);
+		client.setInvoices(invoices);
+
+		invoice1.setClient(client);
+		invoice2.setClient(client);
+
+		clientRepository.save(client);
+		System.out.println(client);
 	}
 
 }
