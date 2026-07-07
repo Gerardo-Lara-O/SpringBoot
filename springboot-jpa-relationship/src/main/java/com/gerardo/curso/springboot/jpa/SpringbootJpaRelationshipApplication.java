6ec90542e77d211df7ed32c4303155a4f414af2a
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2026-07-06 20:00:45 -0600

    Codificando ejemplo ManyToOne con relacion existente y anotacion @JoinColumn

--- springboot-jpa-relationship/src/main/java/com/gerardo/curso/springboot/jpa/SpringbootJpaRelationshipApplication.java
+++ springboot-jpa-relationship/src/main/java/com/gerardo/curso/springboot/jpa/SpringbootJpaRelationshipApplication.java
@@ -1,13 +1,63 @@
 package com.gerardo.curso.springboot.jpa;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.gerardo.curso.springboot.jpa.models.Client;
+import com.gerardo.curso.springboot.jpa.models.Invoice;
+import com.gerardo.curso.springboot.jpa.repositories.ClientRepository;
+import com.gerardo.curso.springboot.jpa.repositories.InvoiceRepository;
+
 @SpringBootApplication
-public class SpringbootJpaRelationshipApplication {
+public class SpringbootJpaRelationshipApplication implements CommandLineRunner{
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private InvoiceRepository invoiceRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		manyToOneFindByIdClient();
+	}
+
+	public void manyToOne(){
+
+		Client client = new Client("John", "Doe");
+		clientRepository.save(client);
+
+		Invoice invoice = new Invoice("Compras de oficina", 2000L);
+		invoice.setClient(client);
+		Invoice invoiceDb = invoiceRepository.save(invoice);
+		System.out.println(invoiceDb);
+
+	}
+
+		public void manyToOneFindByIdClient(){
+
+			Optional<Client> optionalClient = clientRepository.findById(2L);
+
+			if (optionalClient.isPresent()) {
+				Client client = optionalClient.orElseThrow();
+				Invoice invoice = new Invoice("Compras de oficina", 2000L);
+				invoice.setClient(client);
+				Invoice invoiceDb = invoiceRepository.save(invoice);
+				System.out.println(invoiceDb);
+			}
+
+
+		
+
 	}
 
 }
